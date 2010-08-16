dc3fb8daf9f1968aaa562c9e7bd6e3d0bf1f27dd
Greg Scott <i@gregoryjscott.com>
2010-08-16 13:05:13 -0800

    increased allowable parameter length to 128 for now, need to do something better permanently

--- Simpler/Data/Tasks/FindParametersInCommandText.cs
+++ Simpler/Data/Tasks/FindParametersInCommandText.cs
@@ -18,10 +18,10 @@ namespace Simpler.Data.Tasks
 
             // Name the grouping "Parameter", make it start with ":" or "@", then a letter, and followed by up to 29 letters, numbers, or underscores.  Finally, look ahead
             // and make sure the next character is not a letter, number, or underscore.
-            regularExpression.Append(@"(?<Parameter>[:@][a-zA-Z][a-zA-Z0-9_\.]{0,29})(?=[^a-zA-Z0-9_\.])");
+            regularExpression.Append(@"(?<Parameter>[:@][a-zA-Z][a-zA-Z0-9_\.]{0,127})(?=[^a-zA-Z0-9_\.])");
 
             // Or, allow the same thing as above accept look ahead and allow the string to end immediately after the parameter.
-            regularExpression.Append(@"|(?<Parameter>[:@][a-zA-Z][a-zA-Z0-9_\.]{0,29})$");
+            regularExpression.Append(@"|(?<Parameter>[:@][a-zA-Z][a-zA-Z0-9_\.]{0,127})$");
 
             var regex = new Regex(regularExpression.ToString(), RegexOptions.Multiline);
             var matches = regex.Matches(CommandText);
