5a4ff62dce400c0fbe0b0d0e6e949eee256a9a12
James Clancey <james.clancey@gmail.com>
2010-10-26 19:37:13 -0800

    Added failing test for FetchSingleOf.cs when more then one result is found.

--- Simpler.Tests/Data/Tasks/FetchSingleOfTest.cs
+++ Simpler.Tests/Data/Tasks/FetchSingleOfTest.cs
@@ -31,5 +31,36 @@ namespace Simpler.Tests.Data.Tasks
             // Assert
             Assert.That(task.ObjectFetched.Name, Is.EqualTo("John Doe"));
         }
+        [Test]
+        public void should_return_only_one_object()
+        {
+            var task = TaskFactory<FetchSingleOf<MockObject>>.Create();
+
+            var table = new DataTable();
+            table.Columns.Add("Name", Type.GetType("System.String"));
+            table.Columns.Add("Age", Type.GetType("System.Int32"));
+            table.Rows.Add(new object[] { "John Doe", "21" });
+            table.Rows.Add(new object[] { "John Doe", "21" });
+
+            var mockSelectCommand = new Mock<IDbCommand>();
+            mockSelectCommand.Setup(command => command.ExecuteReader()).Returns(table.CreateDataReader());
+            task.SelectCommand = mockSelectCommand.Object;
+
+            // Act
+            bool errorThrown = false;
+            try
+            {
+
+                task.Execute();
+            }
+            catch (Exception)
+            {
+                errorThrown = true;
+            }
+
+            // Assert
+            Assert.That(errorThrown);
+
+        }
     }
 }
