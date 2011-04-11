d5bc9871a6a6e1668c2952a98048b5417d651b3b
Greg Scott <i@gregoryjscott.com>
2011-04-10 20:46:32 -0800

    class comments for remaining classes

--- Simpler/Task.cs
+++ Simpler/Task.cs
@@ -1,7 +1,9 @@
-﻿using System;
-
-namespace Simpler
+﻿namespace Simpler
 {
+    /// <summary>
+    /// Base class for all tasks.  It ensures that all tasks have the Execute() method that
+    /// can be overridden (which allows it to be intercepted in a proxy class).
+    /// </summary>
     public abstract class Task
     {
         public abstract void Execute();
--- Simpler/TaskFactory.cs
+++ Simpler/TaskFactory.cs
@@ -2,6 +2,10 @@
 
 namespace Simpler
 {
+    /// <summary>
+    /// TaskFactory is used to instantiate tasks.
+    /// </summary>
+    /// <typeparam name="T">The type of task that should be created.</typeparam>
     public class TaskFactory<T> where T : Task
     {
         public static T Create()
