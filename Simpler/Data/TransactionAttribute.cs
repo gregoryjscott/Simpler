eb5921e3d8a2906a7039936f9bff2dab9c927dff
Greg Scott <i@gregoryjscott.com>
2011-05-14 13:55:10 -0800

    added [Transaction] that wraps TransactionScope

---
+++ Simpler/Data/TransactionAttribute.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Transactions;
+using Simpler.Construction;
+
+namespace Simpler.Data
+{
+    public class TransactionAttribute : ExecutionCallbacksAttribute
+    {
+        public TransactionAttribute(TimeSpan? timeout = null)
+        {
+            // Default to 30 seconds if a timeout is not supplied.
+            if (timeout == null)
+            {
+                _timeout = TimeSpan.FromSeconds(30);
+            }
+        }
+
+        readonly TimeSpan _timeout;
+        TransactionScope _transactionScope;
+        bool _errorOccurred;
+
+        public override void BeforeExecute(Task taskBeingExecuted)
+        {
+            // Reset the flag.
+            _errorOccurred = false;
+
+            if (_transactionScope == null)
+            {
+                _transactionScope = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions { Timeout = _timeout });
+            }
+        }
+
+        public override void AfterExecute(Task taskBeingExecuted)
+        {
+            if (!_errorOccurred)
+            {
+                _transactionScope.Complete();
+            }
+
+            _transactionScope.Dispose();
+            _transactionScope = null;
+        }
+
+        public override void OnError(Task taskBeingExecuted, Exception exception)
+        {
+            // Set the flag so that the transaction will not be committed.
+            _errorOccurred = true;
+        }
+    }
+}
