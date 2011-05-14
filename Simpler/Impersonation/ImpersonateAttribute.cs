c533b24bcd99e7718c748ed7b0122174c5560c4a
Greg Scott <i@gregoryjscott.com>
2011-05-14 11:55:27 -0800

    new empty attribute

---
+++ Simpler/Impersonation/ImpersonateAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Simpler.Construction;
+using System;
+
+namespace Simpler.Impersonation
+{
+    public class ImpersonateAttribute : ExecutionCallbacksAttribute
+    {
+        public ImpersonateAttribute(dynamic userToImpersonate)
+        {
+            UserName = userToImpersonate.UserName;
+            Password = userToImpersonate.Password;
+            Domain = userToImpersonate.Domain;
+        }
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Domain { get; set; }
+
+        public override void BeforeExecute(Task taskBeingExecuted)
+        {
+        }
+
+        public override void AfterExecute(Task taskBeingExecuted)
+        {
+        }
+
+        public override void OnError(Task taskBeingExecuted, Exception exception)
+        {
+        }
+    }
+}
