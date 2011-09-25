1e7f08d9e1d3c873c844e8653999fc42fe7aefcc
Greg Scott <i@gregoryjscott.com>
2011-09-24 18:36:08 -0800

    make it specific to tasks

--- Simpler/Testing/TestForT.cs
+++ Simpler/Testing/TestForT.cs
@@ -3,7 +3,7 @@
     public delegate T SetupFor<T>();
     public delegate void VerificationFor<T>(T task);
 
-    public class TestFor<T> : Test
+    public class TestFor<T> : Test where T : Task
     {
         public SetupFor<T> Setup { get; set; }
         public VerificationFor<T> Verify { get; set; }
