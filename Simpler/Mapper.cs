69fc83a0287c5d83be147318b2be5880c7b02708
Greg Scott <i@gregoryjscott.com>
2011-12-24 17:19:59 -0900

    simplified the code

--- Simpler/Mapper.cs
+++ Simpler/Mapper.cs
@@ -5,7 +5,7 @@
         /// <summary>
         /// This is effectively an additional overload to AutoMapper's DynamicMap method.
         /// </summary>
-        internal static void DynamicMap<TDestination>(object source, TDestination destination)
+        internal static void Map<TDestination>(object source, TDestination destination)
         {
             var sourceType = source == null ? typeof(object) : source.GetType();
             var destinationType = typeof(TDestination);
--- Simpler/TaskTITO.cs
+++ Simpler/TaskTITO.cs
@@ -6,43 +6,26 @@ namespace Simpler
     [InjectSubTasks]
     public abstract class Task<TI, TO> : Task where TI : class where TO : class
     {
-
-        TI _inputsModel;
-        TO _outputsModel;
-
         public override dynamic Inputs
         {
-            get { return _inputsModel; }
+            get { return InputsModel; }
             set
             {
-                _inputsModel = (TI)Activator.CreateInstance(typeof(TI));
-                Mapper.DynamicMap<TI>(value, _inputsModel);
+                InputsModel = (TI)Activator.CreateInstance(typeof(TI));
+                Mapper.Map<TI>(value, InputsModel);
             }
         }
         public override dynamic Outputs
         {
-            get { return _outputsModel; }
+            get { return OutputsModel; }
             set
             {
-                _outputsModel = (TO)Activator.CreateInstance(typeof(TO));
-                Mapper.DynamicMap<TO>(value, _outputsModel);
+                OutputsModel = (TO)Activator.CreateInstance(typeof(TO));
+                Mapper.Map<TO>(value, OutputsModel);
             }
         }
-        public TI InputsModel
-        {
-            get { return _inputsModel; }
-            set
-            {
-                _inputsModel = value;
-            }
-        }
-        public TO OutputsModel
-        {
-            get { return _outputsModel; }
-            set
-            {
-                _outputsModel = value;
-            }
-        }
+
+        public virtual TI InputsModel { get; set; }
+        public virtual TO OutputsModel { get; set; }
     }
 }
