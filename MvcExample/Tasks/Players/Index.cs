02af35c95914cadfff45aafe17a8030aff541a4f
Greg Scott <i@gregoryjscott.com>
2012-01-04 20:52:33 -0900

    new tasks to run sql using dbproviderfactories

--- MvcExample/Tasks/Players/Index.cs
+++ MvcExample/Tasks/Players/Index.cs
@@ -1,5 +1,4 @@
-﻿using System.Data.SqlServerCe;
-using MvcExample.Models.Players;
+﻿using MvcExample.Models.Players;
 using Simpler;
 using Simpler.Data.Tasks;
 
@@ -7,37 +6,28 @@ namespace MvcExample.Tasks.Players
 {
     public class Index : Task<object, PlayerIndex>
     {
-        public FetchListOf<PlayerIndexItem> FetchPlayers { get; set; }
+        public RunSqlAndReturn<PlayerIndexItem> FetchPlayers { get; set; }
 
         public override void Execute()
         {
-            //var providerName = ConfigurationManager.ConnectionStrings["ExampleData"].ProviderName;
-            //var provider = DbProviderFactories.GetFactory(providerName);
+            FetchPlayers.ConnectionName = "ExampleData";
+            FetchPlayers.Sql =
+                @"
+                select 
+                    Player.FirstName + ' ' + Player.LastName as Name,
+                    Team.Mascot as Team
+                from 
+                    Player
+                    inner join
+                    Team on
+                        Player.TeamId = Team.TeamId
+                ";
+            FetchPlayers.Execute();
 
-            using (var connection = new SqlCeConnection("Data Source=|DataDirectory|ExampleData.sdf"))
-            using (var command = connection.CreateCommand())
-            {
-                connection.Open();
-                command.Connection = connection;
-                command.CommandText =
-                    @"
-                    select 
-                        Player.FirstName + ' ' + Player.LastName as Name,
-                        Team.Mascot as Team
-                    from 
-                        Player
-                        inner join
-                        Team on
-                            Player.TeamId = Team.TeamId
-                    ";
-
-                FetchPlayers.SelectCommand = command;
-                FetchPlayers.Execute();
-                OutputsModel = new PlayerIndex
-                               {
-                                   PlayerIndexItems = FetchPlayers.ObjectsFetched
-                               };
-            }
+            OutputsModel = new PlayerIndex
+                           {
+                               PlayerIndexItems = FetchPlayers.Models
+                           };
         }
     }
 }
---
+++ Simpler/Data/Tasks/RunSqlAndReturn.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Configuration;
+using System.Data.Common;
+
+namespace Simpler.Data.Tasks
+{
+    // todo - Using Task<TI, TO> here just to get the InjectSubTasks attribute.
+    public class RunSqlAndReturn<TModel> : Task<object, object>
+    {
+        // Inputs
+        public string ConnectionName { get; set; }
+        public string Sql { get; set; }
+        public object Values { get; set; }
+
+        // Outputs
+        public TModel[] Models { get; private set; }
+
+        // Sub-tasks
+        public BuildParameters BuildParameters { get; set; }
+        public FetchListOf<TModel> FetchModels { get; set;}
+
+        public override void Execute()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings[ConnectionName].ConnectionString;
+            var providerName = ConfigurationManager.ConnectionStrings[ConnectionName].ProviderName;
+            var provider = DbProviderFactories.GetFactory(providerName);
+
+            using (var connection = provider.CreateConnection())
+            {
+                if (connection == null) throw new Exception("todo");
+
+                connection.ConnectionString = connectionString;
+
+                using (var command = connection.CreateCommand())
+                {
+                    connection.Open();
+                    command.Connection = connection;
+                    command.CommandText = Sql;
+
+                    BuildParameters.CommandWithParameters = command;
+                    BuildParameters.ObjectWithValues = Values;
+                    BuildParameters.Execute();
+
+                    FetchModels.SelectCommand = command;
+                    FetchModels.Execute();
+                }
+            }
+
+            Models = FetchModels.ObjectsFetched;
+        }
+    }
+}
