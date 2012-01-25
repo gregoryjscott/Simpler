804de7c7d6b982fc36dc06fb9d434d81dbd4a9a5
Greg Scott <i@gregoryjscott.com>
2012-01-24 17:11:09 -0900

    cleanup

--- Simpler/Web/Models/IndexResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Simpler.Web.Models
-{
-    public class IndexResult<TModel>
-    {
-        public bool ErrorOccurred { get; set; }
-        public string ErrorMessage { get; set; }
-        public TModel Model { get; set; }
-    }
-}
