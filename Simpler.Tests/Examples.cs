a9e813203587059e9dd9609072d9d485047032de
Greg Scott <i@gregoryjscott.com>
2012-05-24 15:58:33 -0800

    update the docs (um... just the README)

--- Simpler.Tests/Examples.cs
+++ Simpler.Tests/Examples.cs
@@ -1,14 +1,11 @@
-#Simpler
+﻿using System.Data.SqlClient;
+using NUnit.Framework;
+using Simpler.Data.Tasks;
+using Simpler.Injection;
+using Simpler.Tests.Mocks;
 
-You probably won't like Simpler.  If you enjoy spending your time configuring ORMs, interfacing with DI/IOC frameworks, generating code, and building complex domain models, then you will probably hate Simpler.  Simpler's primary goal is help developers create quick, simple solutions while writing the least amount of code possible.  Every piece of code in an application should have a clearly visible business purpose - the rest is just noise.
-
----
-
-###"What is it?"
-
-For the most part, Simpler is just a philosophy on .NET class design.  All classes that contain functionality are defined as Tasks, named as verbs.  A Task has optional input and/or outputs (POCOs), along with a single Execute() method - and that's it.  Simpler comes with a Task base class, a static TaskFactory class for instantiating Tasks, along with various built-in Tasks that you can use as sub-tasks in your Tasks (see the second example).
-
-	'
+namespace Simpler
+{
     class Ask : Task
     {
         // Inputs
@@ -25,15 +22,7 @@ For the most part, Simpler is just a philosophy on .NET class design.  All class
                 : "Get a life.";
         }
     }
-	'
 
----
-
-###"What's the purpose of the TaskFactory?"
-
-TaskFactory appears to just return an instance of the given Task type, but it actually returns a proxy to the Task. The proxy allows for intercepting Task Execute() calls and performing actions before and/or after the Task execution. For example, the built-in InjectSubTasks attribute will automatically instantiate sub-task properties (only if null) before Task execution, and automatically dispose of them after execution.
-
-	'
     [InjectSubTasks]
     class BeAnnoying : Task
     {
@@ -61,17 +50,7 @@ TaskFactory appears to just return an instance of the given Task type, but it ac
             beAnnonying.Execute();
         }
     }
-	'
 
-Sub-task injection simplifies the code, but more importantly it allows for mocking sub-tasks as necessary in Task unit tests.
-
----
-
-###"What about database interaction?"
-
-Simpler provides a small set of Tasks for interacting with System.Data.IDbCommand. Using SQL, Simpler makes it pretty easy to get data out of a database and into .NET objects, or persist data from .NET objects to a database.
-
-	'
     class SomePoco 
     {
         public bool AmIImportant { get; set; }
@@ -118,15 +97,7 @@ Simpler provides a small set of Tasks for interacting with System.Data.IDbComman
             }
         }
     }
-	'
 
-Simpler isn't a full-featured ORM, but it gets the job done.
-
----
-
-###"You do write tests, don't you?"
-
-	'
     [TestFixture]
     public class FetchSomeStuffTest
     {
@@ -143,12 +114,4 @@ Simpler isn't a full-featured ORM, but it gets the job done.
             Assert.That(task.SomePocos.Length, Is.EqualTo(9));
         }
     }
-	'
-
-By design, all classes clearly define their inputs, outputs, and code to test, so test are real straightforward.
-
----
-
-Simpler is a tool for developing applications as sets of consistent, discrete, interchangable classes that aren't THINGS, but rather DO THINGS. Simpler works great in team environments because all developers on the team design classes using the same techniques and terminology, resulting in a consistent code base. Simpler fits like a glove with a ASP.NET MVC (add a Tasks folder next to your Controllers, Models, and Views folders). And finally, Simpler can be installed using NuGet.
-
-**Simpler is licensed under the MIT License.  A copy of the MIT license can be found in the LICENSE file.**
+}
