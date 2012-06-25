34ae00d9a7c910a572fe8fe7a49cc2338f0589ed
Greg Scott <i@gregoryjscott.com>
2012-06-24 18:30:18 -0800

    comments

--- Example.Model/Config.cs
+++ Example.Model/Config.cs
@@ -4,6 +4,9 @@ namespace Example.Model
 {
     public static class Config
     {
+        /// <summary>
+        /// DataDirectory is set automatically in the web application, but must be set manually for testing, etc.
+        /// </summary>
         public static void SetDataDirectory()
         {
             AppDomain.CurrentDomain.SetData("DataDirectory", AppDomain.CurrentDomain.BaseDirectory + @"\App_Data");
