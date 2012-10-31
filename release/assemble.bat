425aa3577d86cc23b5a10b499155812ed5db1b1d
Greg Scott <i@gregoryjscott.com>
2012-10-30 17:24:13 -0800

    bump version

--- release/assemble.bat
+++ release/assemble.bat
@@ -3,6 +3,7 @@ del /s /q release\template\lib\*.*
 
 echo build
 call build\release
+if %ERRORLEVEL% GTR 0 exit /B %ERRORLEVEL%
 
 echo copy files to release
 xcopy app\Simpler\bin\Release\Simpler.dll release\template\lib /y /i
