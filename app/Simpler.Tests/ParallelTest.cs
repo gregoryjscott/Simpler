e2d0be5d1daa2e49f083f8ce4e67ead369ff3fb9
Greg Scott <i@gregoryjscott.com>
2013-02-19 21:01:45 -0900

    add static class for executing tasks in parallel

---
+++ app/Simpler.Tests/ParallelTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+using Simpler.Tests.Core.Mocks;
+
+namespace Simpler.Tests
+{
+    [TestFixture]
+    public class ParallelTest
+    {
+        [Test]
+        public void can_run_tasks_faster_in_parallel()
+        {
+            // Arrange
+            Task[] tasks = new[] { Task.New<MockSlowTask>(), Task.New<MockSlowTask>() };
+
+            // Act
+            var stopwatch = Stopwatch.StartNew();
+            foreach (var task in tasks)
+            {
+                task.Execute();
+            }
+            var sequential = stopwatch.ElapsedMilliseconds;
+
+            stopwatch.Restart();
+            Parallel.Execute(tasks);
+            var parallel = stopwatch.ElapsedMilliseconds;
+
+            // Assert
+            Assert.That(parallel, Is.LessThan(sequential));
+        }
+    }
+}
---
+++ app/Simpler/Parallel.cs
@@ -0,0 +1,10 @@
+﻿namespace Simpler
+{
+    public static class Parallel
+    {
+        public static void Execute(params Task[] tasks)
+        {
+            System.Threading.Tasks.Parallel.ForEach(tasks, task => task.Execute());
+        }
+    }
+}
