ba9e99e88d0b81d9f969554535bf65206623ec8f
Greg Scott <i@gregoryjscott.com>
2013-07-14 12:55:44 -0800

    bump to 2.3.1

--- app/Simpler/Properties/AssemblyInfo.cs
+++ app/Simpler/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 
@@ -36,5 +36,5 @@ using System.Runtime.CompilerServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.3.0.*")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.3.1.*")]
+[assembly: AssemblyFileVersion("2.3.1.0")]
