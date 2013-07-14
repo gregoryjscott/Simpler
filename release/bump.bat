7ce7d0b391113fa2b8391392f4465470c0188eeb
Greg Scott <i@gregoryjscott.com>
2013-07-14 12:55:09 -0800

    add bump script

---
+++ release/bump.bat
@@ -0,0 +1,3 @@
+release\tools\please.exe bump %1 version in app\Simpler\Properties\AssemblyInfo.cs
+release\tools\please.exe bump %1 version in release\push.bat
+release\tools\please.exe bump %1 version in release\template\Simpler.nuspec
