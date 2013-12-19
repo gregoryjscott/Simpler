3242bf5ed867f148a89298f82a4ecfe4562fc48f
Greg Scott <i@gregoryjscott.com>
2013-12-18 19:26:38 -0900

    cleanup

--- app/Simpler/Data/Tasks/BuildDynamic.cs
+++ app/Simpler/Data/Tasks/BuildDynamic.cs
@@ -17,38 +17,17 @@ namespace Simpler.Data.Tasks
             public dynamic Object { get; set; }
         }
 
-        class Dynamic : DynamicObject
-        {
-            readonly Dictionary<string, object> _members = new Dictionary<string, object>();
-
-            public override bool TryGetMember(GetMemberBinder binder, out object result)
-            {
-                return _members.TryGetValue(binder.Name, out result);
-            }
-
-            public override bool TrySetMember(SetMemberBinder binder, object value)
-            {
-                _members[binder.Name.ToLower()] = value;
-                return true;
-            }
-
-            public void AddMember(string memberName, object memberValue)
-            {
-                _members[memberName] = memberValue;
-            }
-        }
-
         public override void Execute()
         {
             Out.Object = new ExpandoObject();
-            var OutObject = Out.Object as IDictionary<string, Object>;
+            var dictionary = Out.Object as IDictionary<string, Object>;
             for (var i = 0; i < In.DataRecord.FieldCount; i++)
             {
                 var columnName = In.DataRecord.GetName(i);
                 var columnValue = In.DataRecord[columnName];
-                if (columnValue.GetType() != typeof(System.DBNull))
+                if (columnValue.GetType() != typeof(DBNull))
                 {
-                    OutObject.Add(columnName, columnValue);
+                    dictionary.Add(columnName, columnValue);
                 }
             }
         }
