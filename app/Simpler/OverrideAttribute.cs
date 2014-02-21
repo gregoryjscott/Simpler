d5573c4db7aa818b0233d9cdab012b1c5230a05a
Greg Scott <i@gregoryjscott.com>
2014-02-20 19:38:11 -0900

    bring back tests for execution events

---
+++ app/Simpler/OverrideAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Simpler
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public abstract class OverrideAttribute : Attribute
+    {
+        //public abstract void ExecuteOverride(IInvocation execute);
+    }
+}
