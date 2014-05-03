bec94077645c57352ad4aa68ee483a2f3e0d43d4
Greg Scott <i@gregoryjscott.com>
2014-05-02 18:06:21 -0800

    Delete obsolete scripts

--- release/bump.bat
+++
@@ -1,3 +0,0 @@
-release\tools\please.exe bump %1 version in app\Simpler\Properties\AssemblyInfo.cs
-release\tools\please.exe bump %1 version in release\push.bat
-release\tools\please.exe bump %1 version in release\template\Simpler.nuspec
--- release/push.bat
+++
@@ -1,3 +0,0 @@
-release\tools\NuGet.exe push release\output\Simpler.2.3.1.nupkg
-
-exit /B %ERRORLEVEL%
