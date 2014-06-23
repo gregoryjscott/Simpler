f17b5369cf8f9a921ac19a4d9ee3a3a685318380
Greg Scott <i@gregoryjscott.com>
2014-06-22 21:52:31 -0600

    Add tests for FakeTask

--- api/Tests/Simpler/Core/Tasks/FakeTastTest.cs
+++ api/Tests/Simpler/Core/Tasks/FakeTastTest.cs
@@ -1,9 +1,36 @@
 ﻿using NUnit.Framework;
+using Simpler;
+using Simpler.Core.Tasks;
+using Simpler.Mocks;
+using System;
 
 namespace Simpler.Core.Tasks
 {
     [TestFixture]
     public class FakeTaskTest
     {
+        [Test]
+        public void should_create_instance_of_given_type()
+        {
+            var fakeTask = Task.New<FakeTask>();
+            fakeTask.In.TaskType = typeof(MockTask);
+            fakeTask.Execute();
+
+            Assert.That(fakeTask.Out.TaskInstance, Is.InstanceOf<MockTask>());
+        }
+
+        [Test]
+        public void should_override_execute_with_given_action()
+        {
+            var overrideCalled = false;
+            Action<Task> action = t => overrideCalled = true;
+            var fakeTask = Task.New<FakeTask>();
+            fakeTask.In.TaskType = typeof(MockTask);
+            fakeTask.In.ExecuteOverride = action;
+            fakeTask.Execute();
+            (fakeTask.Out.TaskInstance as MockTask).Execute();
+
+            Assert.That(overrideCalled, Is.True);
+        }
     }
 }
